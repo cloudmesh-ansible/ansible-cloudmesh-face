4a1e208ce89c1356bb307a4960f4eb6788fecf6a
Sonal shrivastava <sshrivas@iu.edu>
2016-04-13 00:25:24 -0400

    Updated script

--- bin/demo2.sh
+++ bin/demo2.sh
@@ -1,2 +1,2 @@
-./demos/compare.py images/examples/{lennon*,clapton*}
+echo "timetype,value" > Comparer_time_results.csv &&  (time -p ./demos/compare.py images/examples/{lennon*,clapton*}| tail -n 3) 2>> Comparer_time_results.csv && sed -i "s/\ /,/g" Comparer_time_results.csv
 
