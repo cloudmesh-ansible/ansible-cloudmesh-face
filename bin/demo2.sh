dd0138a4b982b740ecbff79af21b6868058fd8ae
Sonal shrivastava <sshrivas@iu.edu>
2016-04-14 12:38:49 -0400

    Running script for 10 times and recording output in compare_output.txt and time in compare_time.csv

--- bin/demo2.sh
+++ bin/demo2.sh
@@ -1,2 +1,1 @@
-echo "timetype,value" > Comparer_time_results.csv &&  (time -p ./demos/compare.py images/examples/{lennon*,clapton*}| tail -n 3) 2>> Comparer_time_results.csv && sed -i "s/\ /,/g" Comparer_time_results.csv
-
+echo "real,user,sys" > compare_time.csv && (for i in `seq 10`; do TIMEFORMAT=%R','%U','%S && time ./demos/compare.py images/examples/{lennon*,clapton*};done)>compare_output.txt  2>> compare_time.csv
