356bd0f1516ddfd2b39b36680f034556c1a6098a
shrivastavasonal7 <shrivastavasonal7@gmail.com>
2016-04-19 00:40:06 -0400

    update:dev: Added command to change the folder to openface at  last

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -33,3 +33,5 @@ echo "Automated Docker build for OpenFace"
     docker pull bamos/openface
     
     docker run -p 9000:9000 -p 8000:8000 -t -i bamos/openface /bin/bash
+    
+    cd /root/src/openface
