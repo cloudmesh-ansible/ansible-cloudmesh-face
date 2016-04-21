d02706588bf6f923b1e2eee3c6dbbba8dbbe6495
Sonal shrivastava <sshrivas@iu.edu>
2016-04-20 21:10:25 -0400

    Update:dev:set the path for the csv files to ~/ansible-cloudmesh-face/performance

--- scripts/eval.py
+++ scripts/eval.py
@@ -3,17 +3,18 @@ import pandas as pd
 import glob
 import csv
 import os
-files = glob.glob ('/Users/Sonal/Desktop/sample/*.csv')
+
+files = glob.glob ('~/ansible-cloudmesh-face/performance/*.csv')
 print files
 
 data = pd.DataFrame()
 
-path = '/Users/Sonal/Desktop/sample'
+'''path = '/Users/Sonal/Desktop/sample'
 dirList = os.listdir(path)
 for dl in dirList:
      fullpath=os.path.join(path,dl)
      print fullpath	
-     print dl
+     print dl'''
 
 
 for file in files:
@@ -34,30 +35,3 @@ data.boxplot(by='file')
 
 plt.show()
 
-'''def run(fullpath,dl):
-    data = pd.DataFrame()
-    with open(fullpath, "rb") as infile:
-	#for file in infile:
-        df = pd.read_csv(file, sep=',')
-    	df['file'] = dl
-    	data = pd.concat([data, df])
-        print df	
-
-	LABELS = ["real", "user", "sys"]
-
-	plt.title('Time Taken by Classifier')
-	plt.xlabel('Time_Types')
-	plt.ylabel('Time_Value in (sec)')
-
-
-	data.boxplot(by='file')
-
-	plt.show()
-
-if __name__ == "__main__":
-	path = "/Users/Sonal/Desktop/sample"
-	dirList = os.listdir(path)
-	print dirList
-	for dl in dirList:
-     	    fullpath=os.path.join(path,dl)
-            run(fullpath,dl)'''
