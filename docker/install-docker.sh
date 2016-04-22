23d182a724bf4d8a73714af43e4fa0d2b3ec86b5
shrivastavasonal7 <shrivastavasonal7@gmail.com>
2016-04-22 01:02:08 -0400

    Update:dev: working out the last cd command

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -36,4 +36,4 @@ echo "Automated Docker build for OpenFace"
     
     docker run -p 9000:9000 -p 8000:8000 -t -i bamos/openface /bin/bash
     
-    alias proj="cd /root/src/openface"
+   /bin/sh -c 'cd /root/src/openface'
