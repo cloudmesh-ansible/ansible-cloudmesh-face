3cad93076fef75115838dba1df5b3823937b9334
shrivastavasonal7 <shrivastavasonal7@gmail.com>
2016-04-22 01:13:12 -0400

    Update install-docker.sh

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -34,6 +34,6 @@ echo "Automated Docker build for OpenFace"
     
     docker pull bamos/openface
     
-    docker run -p 9000:9000 -p 8000:8000 -t -i bamos/openface /bin/bash
+    docker run -w -p 9000:9000 -p 8000:8000 -t -i bamos/openface /bin/bash
     
    /bin/bash -c 'cd /root/src/openface'
