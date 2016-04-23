81529a68a1539cbdf274a9672464ea2d78065608
Sonal shrivastava <sshrivas@iu.edu>
2016-04-23 14:02:29 -0400

    Update:dev: number of experiments as parameter

--- docker/demo3.sh
+++ docker/demo3.sh
@@ -1,1 +1,2 @@
-echo "real,user,sys" > docker_classifier_time.csv && (for i in `seq 10`; do TIMEFORMAT=%R','%U','%S && time ./demos/classifier.py infer ./models/openface/celeb-classifier.nn4.small2.v1.pkl images/examples/{carell,adams,lennon}* ;done)>classifier_output.txt  2>> docker_classifier_time.csv
+S=`seq 50`
+echo "real,user,sys" > docker_classifier_time.csv && (for i in $S; do TIMEFORMAT=%R','%U','%S && time ./demos/classifier.py infer ./models/openface/celeb-classifier.nn4.small2.v1.pkl images/examples/{carell,adams,lennon}* ;done)>classifier_output.txt  2>> docker_classifier_time.csv
