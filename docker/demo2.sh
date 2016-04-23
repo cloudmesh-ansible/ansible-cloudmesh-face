b2b835b6e995e1b26a937da013208cab72976c98
Sonal shrivastava <sshrivas@iu.edu>
2016-04-23 14:01:54 -0400

    Update:dev: number of experiments as parameter

--- docker/demo2.sh
+++ docker/demo2.sh
@@ -1,1 +1,2 @@
-echo "real,user,sys" > docker_compare_time.csv && (for i in `seq 10`; do TIMEFORMAT=%R','%U','%S && time ./demos/compare.py images/examples/{lennon*,clapton*};done)>compare_output.txt  2>> docker_compare_time.csv
+S=`seq 50`
+echo "real,user,sys" > docker_compare_time.csv && (for i in $S; do TIMEFORMAT=%R','%U','%S && time ./demos/compare.py images/examples/{lennon*,clapton*};done)>compare_output.txt  2>> docker_compare_time.csv
