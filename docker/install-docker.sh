347124acda6d6e777ade10c36bf8d8194f8ec368
Sonal shrivastava <sshrivas@iu.edu>
2016-04-23 10:31:22 -0400

    Modified to name and use the docker

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -26,17 +26,17 @@ echo "Automated Docker build for OpenFace"
     
     docker pull bamos/openface
 
-    docker run -d -p 9000:9000 -p 8000:8000  bamos/openface
+    docker run -d  -p 9000:9000 -p 8000:8000 --name openface   bamos/openface
     
-    SS=`docker ps --format "{{.ID}}"`
+    SS=`docker ps --format "{{.Names}}"`
     
-    echo "Container ID is:"$SS
+    echo "Container Name is:"$SS
     
     echo "Copying demo2.sh and demo3.sh"
 
-    docker cp demo2.sh $SS:/root/src/openface/demo2.sh
+    docker cp demo2.sh openface:/root/src/openface/demo2.sh
 
-    docker cp demo3.sh $SS:/root/src/openface/demo3.sh
+    docker cp demo3.sh openface:/root/src/openface/demo3.sh
 
     docker exec -t -i $SS /bin/bash
  
