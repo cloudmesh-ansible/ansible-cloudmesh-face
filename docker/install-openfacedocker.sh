bef606c2ebe875982fd1a6078bddfe2ad2bdee34
Sonal shrivastava <sshrivas@iu.edu>
2016-04-29 17:51:59 -0400

    Using Volume in docker to share files

--- docker/install-openfacedocker.sh
+++ docker/install-openfacedocker.sh
@@ -6,8 +6,6 @@ echo "Automated Docker build for OpenFace"
 
     docker run -p 9000:9000 -p 8000:8000 --name openface -v /Users/Sonal/ansible-cloudmesh-face/docker:/root/src/openface/docker  -ti bamos/openface /bin/bash
 
-    SS=`docker ps --format "{{.Names}}"`
-    
-    echo "Container Name is:"$SS
+   
     
  
