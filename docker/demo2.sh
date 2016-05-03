5caa2a4cb88e99a0c791bf76493cdcceb162ec44
Sonal shrivastava <sshrivas@iu.edu>
2016-05-03 00:38:32 -0400

    update:dev:added container ids

--- docker/demo2.sh
+++ docker/demo2.sh
@@ -1,2 +1,5 @@
-S=`seq 50`
-echo "real,user,sys" > docker_compare_time.csv && (for i in $S; do TIMEFORMAT=%R','%U','%S && time ./demos/compare.py images/examples/{lennon*,clapton*};done)>compare_output.txt  2>> docker_compare_time.csv
+S=$2
+CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut -c -12)"
+echo "Output generated for Container :"$CID" !! Please verify the csv and txt files using ls -l"
+
+echo "real,user,sys" > docker_compare_$CID.csv && (for i in $S; do TIMEFORMAT=%R','%U','%S && time ./demos/compare.py images/examples/{lennon*,clapton*};done)>docker_compare_$CID.txt  2>> docker_compare_$CID.csv
--- docker/demo3.sh
+++ docker/demo3.sh
@@ -1,2 +1,6 @@
-S=`seq 50`
-echo "real,user,sys" > docker_classifier_time.csv && (for i in $S; do TIMEFORMAT=%R','%U','%S && time ./demos/classifier.py infer ./models/openface/celeb-classifier.nn4.small2.v1.pkl images/examples/{carell,adams,lennon}* ;done)>classifier_output.txt  2>> docker_classifier_time.csv
+S=$2
+CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut -c -12)"
+
+echo "Output generated for Container :"$CID" !! Please verify the csv and txt files using ls -l"
+
+echo "real,user,sys" > docker_classifier_$CID.csv && (for i in $S; do TIMEFORMAT=%R','%U','%S && time ./demos/classifier.py infer ./models/openface/celeb-classifier.nn4.small2.v1.pkl images/examples/{carell,adams,lennon}* ;done)>docker_classifier_$CID.txt  2>> docker_classifier_$CID.csv
