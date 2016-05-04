2db3c55ddc949942db1cf7f8850c5971661c31c9
Sonal shrivastava <sshrivas@iu.edu>
2016-05-04 01:47:37 -0400

    Updated

--- docker/demo2.sh
+++ docker/demo2.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+if [ -z "$*" ]; then echo "Usage: ./sh <NUMBER OF TIMES SCRIPTS TO BE RUN>";
+
+else
 
 CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut -c -12)"
 echo "Generating Output!!!"
@@ -5,4 +10,5 @@ echo "Generating Output!!!"
 echo "real,user,sys" > docker_compare_$CID.csv && (for i in `seq $1`; do TIMEFORMAT=%R','%U','%S && time ../demos/compare.py ../images/examples/{lennon*,clapton*};
 done)>docker_compare_$CID.txt  2>> docker_compare_$CID.csv
 
-echo "Output generated for Container :"$CID" !! Please verify the csv and txt files using ls -l"
+echo "Output generated for Container :"$CID" !! Please verify the csv and txt files using ls -l";
+fi
--- docker/demo3.sh
+++ docker/demo3.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+if [ -z "$*" ]; then echo "Usage: ./sh <NUMBER OF TIMES SCRIPTS TO BE RUN>";
+
+else
 
 CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut -c -12)"
 
@@ -5,4 +10,6 @@ echo "Generating output!!!"
 
 echo "real,user,sys" > docker_classifier_$CID.csv && (for i in `seq $1`; do TIMEFORMAT=%R','%U','%S && time ../demos/classifier.py infer ../models/openface/celeb-classifier.nn4.small2.v1.pkl ../images/examples/{carell,adams,lennon}* ;done)>docker_classifier_$CID.txt  2>> docker_classifier_$CID.csv
 
-echo "Output generated for Container :"$CID" !! Please verify the csv and txt files using ls -l"
+echo "Output generated for Container :"$CID" !! Please verify the csv and txt files using ls -l";
+
+fi
