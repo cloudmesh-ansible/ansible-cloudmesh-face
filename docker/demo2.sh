6205de8e30ac6e7819634b4a362885481c08ef99
Sonal shrivastava <sshrivas@iu.edu>
2016-05-04 00:32:15 -0400

    updates

--- docker/demo2.sh
+++ docker/demo2.sh
@@ -1,5 +1,8 @@
-S=$2
-CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut -c -12)"
-echo "Output generated for Container :"$CID" !! Please verify the csv and txt files using ls -l"
 
-echo "real,user,sys" > docker_compare_$CID.csv && (for i in $S; do TIMEFORMAT=%R','%U','%S && time ./demos/compare.py images/examples/{lennon*,clapton*};done)>docker_compare_$CID.txt  2>> docker_compare_$CID.csv
+CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut -c -12)"
+echo "Generating Output!!!"
+
+echo "real,user,sys" > docker_compare_$CID.csv && (for i in `seq $1`; do TIMEFORMAT=%R','%U','%S && time ../demos/compare.py ../images/examples/{lennon*,clapton*};
+done)>docker_compare_$CID.txt  2>> docker_compare_$CID.csv
+
+echo "Output generated for Container :"$CID" !! Please verify the csv and txt files using ls -l"
