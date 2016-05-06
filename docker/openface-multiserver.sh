95c53e2d768be18f8a434a3d28a939339847b0f8
Sonal shrivastava <sshrivas@iu.edu>
2016-05-06 18:19:18 -0400

    update

--- docker/openface-multiserver.sh
+++ docker/openface-multiserver.sh
@@ -1,20 +1,29 @@
-docker-machine create -d virtualbox openface-machine
-eval "$(docker-machine env openface-machine)"
-docker run -d -p "8500:8500" -h "consul" progrium/consul -server –bootstrap
+#!/bin/bash
 
+if [ -z "$*" ]; then 
+        echo "Usage: .sh <Number of Swarm Nodes to be created>";
 
-docker-machine create -d virtualbox --swarm --swarm-master --swarm-discovery="consul://$(docker-machine ip openface-machine):8500" --engine-opt="cluster-store=consul://$(docker-machine ip openface-machine):8500" --engine-opt="cluster-advertise=eth1:2376" openface-master
+else 
 
-docker-machine create -d virtualbox --swarm  --swarm-discovery="consul://$(docker-machine ip openface-machine):8500" --engine-opt="cluster-store=consul://$(docker-machine ip openface-machine):8500" --engine-opt="cluster-advertise=eth1:2376" openface-node1
+	docker-machine create -d virtualbox openface-machine
+	eval "$(docker-machine env openface-machine)"
+	docker run -d -p "8500:8500" -h "consul" progrium/consul -server –bootst	rap
+	
+	docker-machine create -d virtualbox --swarm --swarm-master --swarm-discovery="consul://$(docker-machine ip openface-machine):8500" --engine-opt="cluster-store=consul://$(docker-machine ip openface-machine):8500" --engine-opt="cluster-advertise=eth1:2376" openface-master	
+	
+	for ((i = 1; i <= $1; i++)); do
+		
+		docker-machine create -d virtualbox --swarm  --swarm-discovery="consul://$(docker-machine ip openface-machine):8500" --engine-opt="cluster-store=consul://$(docker-machine ip openface-machine):8500" --engine-opt="cluster-advertise=eth1:2376" openface-node-$i;        
+	done	
+	eval $(docker-machine env --swarm openface-master)
+	docker pull bamos/openface
 
+	for machine in $(docker-machine ls | tail -n +2 | awk '{ print $1 }'); do
+  		eval $(docker-machine env $machine)
 
-eval $(docker-machine env --swarm openface-master)
-docker pull bamos/openface
+    		docker run -p 9000:9000 -p 8000:8000 --name openface -v /Users/Sonal/ansible-cloudmesh-face/docker:/root/openface/docker  -ti bamos/openface /bin/bash  
 
-for machine in $(docker-machine ls | tail -n +2 | awk '{ print $1 }'); do
-  eval $(docker-machine env $machine)
+	done
 
-    docker run -p 9000:9000 -p 8000:8000 --name openface -v /Users/Sonal/ansible-cloudmesh-face/docker:/root/src/openface/docker  -ti bamos/openface /bin/bash
-done
-
-
+	docker-machine rm $(docker-machine ls -q)
+fi
