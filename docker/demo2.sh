62040e4fa9a1c945e5937f4f0eff0b8f3c670b7c
Sonal shrivastava <sshrivas@iu.edu>
2016-05-07 19:52:10 -0400

    update

--- docker/demo2.sh
+++ docker/demo2.sh
@@ -5,6 +5,7 @@ if [ -z "$*" ]; then echo "Usage: ./sh <NUMBER OF TIMES SCRIPTS TO BE RUN>";
 else
 
 CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut -c -12)"
+
 echo "Generating Output!!!"
 
 echo "real,user,sys" > docker_compare_$CID.csv && (for i in `seq $1`; do TIMEFORMAT=%R','%U','%S && time ../demos/compare.py ../images/examples/{lennon*,clapton*};
