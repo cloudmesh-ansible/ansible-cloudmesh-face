1145d283e9238279650150212386534fe507b89d
Gregor von Laszewski <laszewski@gmail.com>
2016-05-10 14:09:34 -0400

    new:dev: completing the gregor demo analysis script

--- performance/gregor_demo2.py
+++ performance/gregor_demo2.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python
 
 
-"""Usage: gregor_demo.py --os=OS --host=HOST
+"""Usage: gregor_demo.py --kind=KIND --os=OS --host=HOST
 
 Arguments:
   HOST     Host name
-  OS OS name
+  OS       OS name
+  KIND     category or classifier
 Options:
   -h --help
   -o       OS
@@ -17,211 +18,96 @@ import glob
 from cloudmesh_client.common.hostlist import Parameter
 import os.path
 import textwrap
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+
+import warnings;
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore");
+    import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
     print(arguments)
 
-    kinds = ['classifier', 'compare']
+    kind = arguments['--kind']
 
     hosts = Parameter.expand(arguments["--host"])
     oses = Parameter.expand(arguments["--os"])
 
-    print (hosts)
-    print (oses)
+    print(hosts)
+    print(oses)
 
     #
     # CLEAN
     #
     for host in hosts:
         for osystem in oses:
-            for kind in kinds:
-                data = {
-                    "os": osystem,
-                    "host": host,
-                    "kind": kind
-                }
+            data = {
+                "os": osystem,
+                "host": host,
+                "kind": kind
+            }
 
-                name = "{os}_{kind}_{host}.csv".format(**data)
-                if os.path.isfile(name):
-                    os.remove(name)
-                    print ("delete", name)
+            name = "{os}_{kind}_{host}.csv".format(**data)
+            if os.path.isfile(name):
+                os.remove(name)
+                print("delete", name)
     for host in hosts:
         for osystem in oses:
-            for kind in kinds:
-                data = {
-                    "os": osystem,
-                    "host": host,
-                    "kind": kind
-                }
-                data["name"] = "{os}_{kind}_{host}".format(**data)
-                pattern = "{name}_*.csv".format(**data)
-                files = glob.glob(pattern)
-                if files != []:
-                    r = ["real,user,sys\n"]
+            data = {
+                "os": osystem,
+                "host": host,
+                "kind": kind
+            }
+            data["name"] = "{os}_{kind}_{host}".format(**data)
+            pattern = "{name}_*.csv".format(**data)
+            files = glob.glob(pattern)
+            if files != []:
+                r = ["real,user,sys\n"]
 
-                    for f in files:
-                        with open(f) as f:
-                            lines = f.readlines()[1:]
-                        r = r + lines
+                for f in files:
+                    with open(f) as f:
+                        lines = f.readlines()[1:]
+                    r = r + lines
 
-                    print ("Merge Files -> {name}.csv\n".format(**data))
-                    for f in files:
-                        print ('  ', f)
-                    print()
-                    with open("{name}.csv".format(**data), 'w') as f:
-                        f.write(''.join(r))
+                print("Merge Files -> {name}.csv\n".format(**data))
+                for f in files:
+                    print('  ', f)
+                print()
+                with open("{name}.csv".format(**data), 'w') as f:
+                    f.write(''.join(r))
+    # columns = DataFrame()
+    values = []
+    for osystem in oses:
+        for host in hosts:
+            data = {
+                "os": osystem,
+                "host": host,
+                "kind": kind
+            }
+            data["name"] = "{os}_{kind}_{host}.csv".format(**data)
+            if os.path.isfile(data["name"]):
+                d = pd.read_csv("{name}".format(**data))
+                for v in d.real:
+                    data["value"] = v
+                    values.append([data['value'], "{os}_{host}".format(**data)])
 
-'''
+    print(values)
 
-args<-commandArgs(TRUE)
+    df = DataFrame(values)
+    df.columns = ['time', 'host']
+    print(df)
 
-print (args)
+    df.boxplot(column='time',
+               by='host')
 
-summaryPlots <- function()
-{
+    plt.suptitle('Performance Comparison OpenFace: {}'.format(kind))
 
-	#Check for required R packages otherwise install them
-	if("ggplot2" %in% rownames(installed.packages()) == FALSE) {
-		install.packages("ggplot2",repos="http://cran.rstudio.com/")
-	}
-	if("tidyr" %in% rownames(installed.packages()) == FALSE) {
-		install.packages("tidyr",repos="http://cran.rstudio.com/")
-	}
+    plt.savefig("test.pdf")
+    plt.close()
 
-#load packages
-library(ggplot2)
-library(tidyr)
-
-ufiles=list.files(path=".", pattern="^ubuntu_compare.*csv$")
-dfiles=list.files(path=".", pattern="^docker_compare.*csv$")
-
-print (ufiles)
-print (dfiles)
-
-
-
-#
-# concatenate all files from the same host
-#
-# type = compare or classifier
-# host = host
-
-os_cat = function(name){
-  system(sprintf("cat %s", name))
-}
-
-csv_merge = function(os, type, host){
-
-  name = sprintf("%s_%s_%s", os, type, host)
-  filename = sprintf("%s.csv", name)
-  
-  print(sprintf ("NNN %s", name))
-  pattern = paste("^", name, ".*csv$", sep="")
-  
-  files=list.files(path=".", pattern=pattern)
-  print(files)
-
-  system (sprintf("echo 'real,user,sys' > %s", filename))
-  os_cat(filename)
-  
-  for (file in files)
-  {
-      print ("concatenate")
-      print 
-      system(sprintf("tail -1 %s >> %s.csv", file, name))
-  }
-  system(sprintf("cat %s.csv", name))
-}
-#temp_dataset <-read.csv(file, header=TRUE)
-#        f1 <- rbind.data.frame(f1, temp_dataset)
-#        rm(temp_dataset)
-#  
-#        }
-#  }
-#  return (f1)
-#}
-
-
-d = csv_merge("docker", "classifier", "big")
-print (d)
-
-
-# 
-# for (file in dfiles){
-#        
-#   # if the merged dataset doesn't exist, create it
-#   if (!exists("f2")){
-#     f2 <- read.csv(file)
-#   }
-#   else
-#   # if the merged dataset does exist, append to it
-#   if (exists("f2")){
-#     temp_dataset <-read.csv(file)
-#     f2 <- rbind.data.frame(f2, temp_dataset)
-#     rm(temp_dataset)
-#   }
-# }
-# 
-# f1=gather(f1)
-# f2=gather(f2)
-# 
-# f1$OS="Ubuntu"
-# f2$OS="Docker"
-# 
-# f12=rbind(f1,f2)
-# 
-# # Ubuntu vs Docker Plots
-# 
-# library(ggplot2)
-# 
-# # Real time plot
-# 
-# p <- ggplot(f12[f12$key=="real",], aes(x=OS, y=value)) +  geom_boxplot() + labs(title="REAL (Wall Clock) time: Docker vs Ubuntu",x="Operating System", y="Time (in sec) ")
-# p <-p + stat_summary(fun.y=mean, geom="point", shape=23, size=4)
-# 
-# p <- p +
-#   theme_bw() +
-#   theme(axis.line = element_line(colour = "black"),
-#     panel.grid.major = element_blank(),
-#     panel.grid.minor = element_blank(),
-#     panel.background = element_blank(),
-# 	legend.position="none") 
-# 
-# ggsave("demo2_real_plot.png")
-# 
-# # Sys time plot
-# 
-# p <- ggplot(f12[f12$key=="sys",], aes(x=OS, y=value)) +  geom_boxplot() + labs(title="SYS time: Docker vs Ubuntu",x="Operating System", y="Time (in sec) ")
-# p <-p + stat_summary(fun.y=mean, geom="point", shape=23, size=4)
-# 
-# p <- p +
-#   theme_bw() +
-#   theme(axis.line = element_line(colour = "black"),
-#     panel.grid.major = element_blank(),
-#     panel.grid.minor = element_blank(),
-#     panel.background = element_blank(),
-# 	legend.position="none") 
-# 
-# ggsave("demo2_sys_plot.png")
-# 
-# # User time plot
-# 
-# p <- ggplot(f12[f12$key=="user",], aes(x=OS, y=value)) +  geom_boxplot() + labs(title="USER time: Docker vs Ubuntu",x="Operating System", y="Time (in sec) ")
-# p <-p + stat_summary(fun.y=mean, geom="point", shape=23, size=4)
-# 
-# p <- p +
-#   theme_bw() +
-#   theme(axis.line = element_line(colour = "black"),
-#     panel.grid.major = element_blank(),
-#     panel.grid.minor = element_blank(),
-#     panel.background = element_blank(),
-# 	legend.position="none") 
-# 
-# ggsave("demo2_user_plot.png")
-
-}
-
-summaryPlots()
-
-'''
+    os.system("open test.pdf")
