27baa2a77117f4359b49a552837fbb86233a1c55
Gregor von Laszewski <laszewski@gmail.com>
2016-05-10 17:11:42 -0400

    new:dev: include the gather step

--- docker/demo.py
+++ docker/demo.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 """Usage:
       demo.py --kind=KIND --count=N
-      demo.py gather
 
 Arguments:
   HOST     Host name
@@ -24,28 +23,25 @@ if __name__ == '__main__':
 
 
     arg = dotdict({
-        'gather': arguments["gather"],
         'n':  arguments["--count"],
         'kind': arguments["--kind"]
     })
 
-    if arg.gather:
-        os.system("gather-csv.sh")
+
+    if arg.kind == "classifier":
+        arg['prg'] = "demo2.sh"
+    elif arg.kind == "compare":
+        arg['prg'] = "demo3.sh"
     else:
+        print ("ERROR: wrong kind")
+        sys.exit()
 
-        if arg.kind == "classifier":
-            arg['prg'] = "demo2.sh"
-        elif arg.kind == "compare":
-            arg['prg'] = "demo3.sh"
-        else:
-            print ("ERROR: wrong kind")
-            sys.exit()
+    os.system("docker rm openface")
 
-        os.system("docker rm openface")
-
-        os.system("docker run -e HOST=`hostname` "
-                  "-e PATH=$PATH:/root/torch/install/bin/ "
-                  "-p 9000:9000 -p 8000:8000 "
-                  "--name openface "
-                  "-v $PWD:/root/openface/docker "
-                  "-ti bamos/openface /bin/bash -c \"cd /root/openface/docker; bash ./{prg} {n}\" ".format(**arg))
+    os.system("docker run -e HOST=`hostname` "
+              "-e PATH=$PATH:/root/torch/install/bin/ "
+              "-p 9000:9000 -p 8000:8000 "
+              "--name openface "
+              "-v $PWD:/root/openface/docker "
+              "-ti bamos/openface /bin/bash -c \"cd /root/openface/docker; bash ./{prg} {n}\" ".format(**arg))
+    os.system("gather-csv.sh")
