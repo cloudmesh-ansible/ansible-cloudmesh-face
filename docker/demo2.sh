91280e47271ef2c57f92187be97de15ebef1f18b
Gregor von Laszewski <laszewski@gmail.com>
2016-05-10 08:56:47 -0400

    fix:dev: improve demo scripts

--- docker/demo2.sh
+++ docker/demo2.sh
@@ -6,22 +6,22 @@ if [ -z "$*" ]; then echo "Usage: ./sh <NUMBER OF TIMES SCRIPTS TO BE RUN>";
 
 else
 
-CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut -c -12)"
+    CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut -c -12)"
 
-echo "Generating Output ..."
+    echo "Generating Output ..."
 
-echo "real,user,sys" > docker_compare_$CID.csv && \
-(for i in `seq $1`; do \
-    TIMEFORMAT=%R','%U','%S && \
-    time ../demos/compare.py ../images/examples/{lennon*,clapton*}; \
-done) > docker_compare_$CID.txt  2>> docker_compare_$CID.csv
+    echo "real,user,sys" > docker_compare_$CID.csv && \
+    (for i in `seq $1`; do \
+        TIMEFORMAT=%R','%U','%S && \
+        time ../demos/compare.py ../images/examples/{lennon*,clapton*}; \
+    done) > docker_compare_$CID.txt  2>> docker_compare_$CID.csv
 
-echo "Output generated for Container: $CID "
-echo
-echo "Outputfiles generated:"
-echo "docker_compare_$CID.txt"
-echo "docker_compare_$CID.csv"
-echo
-cat docker_compare_$CID.csv
+    echo "Output generated for Container: $CID "
+    echo
+    echo "Outputfiles generated:"
+    echo "docker_compare_$CID.txt"
+    echo "docker_compare_$CID.csv"
+    echo
+    cat docker_compare_$CID.csv
 
 fi
--- docker/demo3.sh
+++ docker/demo3.sh
@@ -4,22 +4,22 @@ if [ -z "$*" ]; then echo "Usage: ./sh <NUMBER OF TIMES SCRIPTS TO BE RUN>";
 
 else
 
-CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut -c -12)"
+    CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut -c -12)"
 
-echo "Generating output ..."
+    echo "Generating output ..."
 
-echo "real,user,sys" > docker_classifier_$CID.csv && \
-(for i in `seq $1`; do \
-  TIMEFORMAT=%R','%U','%S && \
-  time ../demos/classifier.py infer ../models/openface/celeb-classifier.nn4.small2.v1.pkl ../images/examples/{carell,adams,lennon}* \
-;done)>docker_classifier_$CID.txt  2>> docker_classifier_$CID.csv
+    echo "real,user,sys" > docker_classifier_$CID.csv && \
+    (for i in `seq $1`; do \
+      TIMEFORMAT=%R','%U','%S && \
+      time ../demos/classifier.py infer ../models/openface/celeb-classifier.nn4.small2.v1.pkl ../images/examples/{carell,adams,lennon}* \
+    ;done)>docker_classifier_$CID.txt  2>> docker_classifier_$CID.csv
 
-echo "Output generated for Container: $CID"
-echo
-echo "Outputfiles generated:"
-echo "docker_classifier_$CID.txt"
-echo "docker_classifier_$CID.csv"
-echo
-cat docker_compare_$CID.csv
+    echo "Output generated for Container: $CID"
+    echo
+    echo "Outputfiles generated:"
+    echo "docker_classifier_$CID.txt"
+    echo "docker_classifier_$CID.csv"
+    echo
+    cat docker_compare_$CID.csv
 
 fi
