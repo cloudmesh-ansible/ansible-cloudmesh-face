009432795815aaca71736043a554278bba1b2fc2
Gregor von Laszewski <laszewski@gmail.com>
2016-05-11 06:07:49 -0400

    new:dev: reformating for better readability

--- docker/demo2big.sh
+++ docker/demo2big.sh
@@ -8,5 +8,8 @@ CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut 
 
 echo "Generating Output!!!
 
-"echo "real,user,sys" > docker_big_compare_$CID.csv && (for i in `seq $1`; do TIMEFORMAT=%R','%U','%S && time demos/compare.py ~/muct/data/jpg/*.jpg;done) > docker_big_compare_$CID.txt  2>> docker_big_compare_$CID.csv
+"echo "real,user,sys" > docker_big_compare_$CID.csv && \
+(for i in `seq $1`; do \
+    TIMEFORMAT=%R','%U','%S && \
+    time demos/compare.py ~/muct/data/jpg/*.jpg;done) > docker_big_compare_$CID.txt  2>> docker_big_compare_$CID.csv
 fi
--- docker/demo3big.sh
+++ docker/demo3big.sh
@@ -8,6 +8,9 @@ CID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -n1 | cut 
 
 echo "Generating output!!!"
 
-echo "real,user,sys" > docker_big_classifier_$CID.csv && (for i in `seq $1`; do TIMEFORMAT=%R','%U','%S  && time ./demos/classifier.py infer ./models/openface/celeb-classifier.nn4.small2.v1.pkl ../muct/data/jpg/*.jpg 2>/dev/null;done)>docker_big_classifier_$CID.txt  2>> docker_big_classifier_$CID.csv
+echo "real,user,sys" > docker_big_classifier_$CID.csv && \
+(for i in `seq $1`; do \
+TIMEFORMAT=%R','%U','%S  && \
+time ./demos/classifier.py infer ./models/openface/celeb-classifier.nn4.small2.v1.pkl ../muct/data/jpg/*.jpg 2>/dev/null;done)>docker_big_classifier_$CID.txt  2>> docker_big_classifier_$CID.csv
 fi
 
